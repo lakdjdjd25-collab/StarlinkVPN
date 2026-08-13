e0cfa8729ad2fc423cc8f1d63ad58cbc56f9a02c
lakdjdjd25-collab <lakdjdjd25@gmail.com>
2026-08-13 19:01:15 +0330

    Fix Compose Modifier import in reference server list

--- apps/android/src/main/java/org/quickping/app/ui/screens/ReferenceServerList.kt
+++ apps/android/src/main/java/org/quickping/app/ui/screens/ReferenceServerList.kt
@@ -1,13 +1,13 @@
 package org.quickping.app.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Modifier
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.quickping.app.state.QuickPingUiState
 
