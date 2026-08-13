de90453d8551339f6dee2a3eb243ed6be0dc83bc
lakdjdjd25-collab <lakdjdjd25@gmail.com>
2026-08-13 20:14:04 +0330

    Fix scanner options compatibility wrapper

--- apps/android/src/main/java/com/google/android/gms/mlkit/codescanner/CodeScannerAliases.kt
+++ apps/android/src/main/java/com/google/android/gms/mlkit/codescanner/CodeScannerAliases.kt
@@ -1,4 +1,19 @@
 package com.google.android.gms.mlkit.codescanner
 
 typealias GmsBarcodeScanning = com.google.mlkit.vision.codescanner.GmsBarcodeScanning
-typealias GmsBarcodeScannerOptions = com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+
+object GmsBarcodeScannerOptions {
+    class Builder {
+        private val delegate = com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions.Builder()
+
+        fun setBarcodeFormats(format: Int, vararg additionalFormats: Int) = apply {
+            delegate.setBarcodeFormats(format, *additionalFormats)
+        }
+
+        fun enableAutoZoom() = apply {
+            delegate.enableAutoZoom()
+        }
+
+        fun build(): com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions = delegate.build()
+    }
+}
