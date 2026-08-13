fec6c798da356ce1af832b29b9967cbd439331a3
lakdjdjd25-collab <lakdjdjd25@gmail.com>
2026-08-13 20:08:57 +0330

    Fix code scanner package bridge

---
+++ apps/android/src/main/java/com/google/android/gms/mlkit/codescanner/CodeScannerAliases.kt
@@ -0,0 +1,4 @@
+package com.google.android.gms.mlkit.codescanner
+
+typealias GmsBarcodeScanning = com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+typealias GmsBarcodeScannerOptions = com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
