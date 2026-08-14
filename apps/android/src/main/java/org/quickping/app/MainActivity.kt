fbd3edab313dfad866132f03480ca5baf927c732
lakdjdjd25-collab <lakdjdjd25@gmail.com>
2026-08-14 11:16:27 +0330

    Force first-pass IME resize on Android

--- apps/android/src/main/java/org/quickping/app/MainActivity.kt
+++ apps/android/src/main/java/org/quickping/app/MainActivity.kt
@@ -2,6 +2,7 @@ package org.quickping.app
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.dark(Color.rgb(5, 7, 10)),
