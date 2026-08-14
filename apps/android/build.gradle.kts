195d557f42b521dc2e5c98ba4bcb6d8481678501
lakdjdjd25-collab <lakdjdjd25@gmail.com>
2026-08-14 11:19:15 +0330

    Bump Android regression-fix test build to 2.6.4

--- apps/android/build.gradle.kts
+++ apps/android/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.quickping"
         minSdk = 23
         targetSdk = 36
-        versionCode = 160166
-        versionName = "2.6.3"
+        versionCode = 160167
+        versionName = "2.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
