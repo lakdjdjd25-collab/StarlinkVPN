cbf9278802d24c4a56c7786731fd72834e2e9555
lakdjdjd25-collab <lakdjdjd25@gmail.com>
2026-08-14 12:41:26 +0330

    Bump Android version to 2.6.7

--- apps/android/build.gradle.kts
+++ apps/android/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.quickping"
         minSdk = 23
         targetSdk = 36
-        versionCode = 160169
-        versionName = "2.6.6"
+        versionCode = 160170
+        versionName = "2.6.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
