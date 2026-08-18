e18f9d04df3d8206fed7aaed55ecf314b0ce149d
lakdjdjd25-collab <lakdjdjd25@gmail.com>
2026-08-18 13:28:21 +0330

    Fix VLESS UUID validation pattern

--- apps/control-plane/src/lib/manual-vless.ts
+++ apps/control-plane/src/lib/manual-vless.ts
@@ -2,7 +2,7 @@ import { lookup } from "node:dns/promises";
 import { isIP } from "node:net";
 import { singBoxRuntimeConfigSchema } from "./sing-box-config";
 
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i;
+const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const SUPPORTED_TRANSPORTS = new Set(["tcp", "ws", "grpc", "http", "h2", "httpupgrade"]);
 const SUPPORTED_SECURITY = new Set(["none", "tls", "reality"]);
 const SUPPORTED_PACKET_ENCODINGS = new Set(["", "xudp", "packetaddr"]);
